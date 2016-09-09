07c3ad2dd90bd35b50f697cf3565e8dfdd7fd809
kreizbauercom <michael.kreizbauer@hotmail.com>
2016-09-09 10:39:02 +0200

    Fehlerbehebung

--- ISO14443Analyzer/source/ISO14443Analyzer.cpp
+++ ISO14443Analyzer/source/ISO14443Analyzer.cpp
@@ -19,39 +19,40 @@ ISO14443Analyzer::~ISO14443Analyzer()
 
 void ISO14443Analyzer::WorkerThread()
 {
-	mResults.reset( new ISO14443AnalyzerResults( this, mSettings.get() ) );
-	SetAnalyzerResults( mResults.get() );
-	mResults->AddChannelBubblesWillAppearOn( mSettings->mInputChannel );
+
+	mResults.reset(new ISO14443AnalyzerResults(this, mSettings.get()));
+	SetAnalyzerResults(mResults.get());
+	mResults->AddChannelBubblesWillAppearOn(mSettings->mInputChannel);
 	mSampleRateHz = GetSampleRate();
 
-	mSerial = GetAnalyzerChannelData( mSettings->mInputChannel );
+	mSerial = GetAnalyzerChannelData(mSettings->mInputChannel);
 
-	if( mSerial->GetBitState() == BIT_LOW )
+	if (mSerial->GetBitState() == BIT_LOW)
 		mSerial->AdvanceToNextEdge();
 
 	U32 samples_per_bit = mSampleRateHz / mSettings->mBitRate;
-	U32 samples_to_first_center_of_first_data_bit = U32( 1.5 * double( mSampleRateHz ) / double( mSettings->mBitRate ) );
+	U32 samples_to_first_center_of_first_data_bit = U32(1.5 * double(mSampleRateHz) / double(mSettings->mBitRate));
 
-	for( ; ; )
+	for (; ; )
 	{
 		U8 data = 0;
 		U8 mask = 1 << 7;
-		
+
 		mSerial->AdvanceToNextEdge(); //falling edge -- beginning of the start bit
 
 		U64 starting_sample = mSerial->GetSampleNumber();
 
-		mSerial->Advance( samples_to_first_center_of_first_data_bit );
+		mSerial->Advance(samples_to_first_center_of_first_data_bit);
 
-		for( U32 i=0; i<8; i++ )
+		for (U32 i = 0; i<8; i++)
 		{
 			//let's put a dot exactly where we sample this bit:
-			mResults->AddMarker( mSerial->GetSampleNumber(), AnalyzerResults::Dot, mSettings->mInputChannel );
+			mResults->AddMarker(mSerial->GetSampleNumber(), AnalyzerResults::Dot, mSettings->mInputChannel);
 
-			if( mSerial->GetBitState() == BIT_HIGH )
+			if (mSerial->GetBitState() == BIT_HIGH)
 				data |= mask;
 
-			mSerial->Advance( samples_per_bit );
+			mSerial->Advance(samples_per_bit);
 
 			mask = mask >> 1;
 		}
@@ -64,10 +65,11 @@ void ISO14443Analyzer::WorkerThread()
 		frame.mStartingSampleInclusive = starting_sample;
 		frame.mEndingSampleInclusive = mSerial->GetSampleNumber();
 
-		mResults->AddFrame( frame );
+		mResults->AddFrame(frame);
 		mResults->CommitResults();
-		ReportProgress( frame.mEndingSampleInclusive );
+		ReportProgress(frame.mEndingSampleInclusive);
 	}
+
 }
 
 bool ISO14443Analyzer::NeedsRerun()
@@ -77,7 +79,7 @@ bool ISO14443Analyzer::NeedsRerun()
 
 U32 ISO14443Analyzer::GenerateSimulationData( U64 minimum_sample_index, U32 device_sample_rate, SimulationChannelDescriptor** simulation_channels )
 {
-	if( mSimulationInitilized == false )
+		if( mSimulationInitilized == false )
 	{
 		mSimulationDataGenerator.Initialize( GetSimulationSampleRate(), mSettings.get() );
 		mSimulationInitilized = true;
